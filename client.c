a0882179594bdf0162bba9d38ff4153171e83e9c
palomaprates <paloma.prates108@gmail.com>
2024-10-17 19:49:57 +0100

    funcs

--- client.c
+++ client.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 
-int main()
+int	main(int argc, char *argv[])
 {
-	printf("teste112\n");
-	return (0);
+	int	i;
+	i = 0;
+		if (argc == 2)
+		{
+			char *server_pid = argv[1];
+			kill(atoi(server_pid), SIGUSR1);
+		}
 }
--- server.c
+++ server.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
-int	main()
+#include <unistd.h>
+#include <signal.h>
+void	teste1()
 {
-	printf("teste2222\n");
-	return (0);
+	write(1, "1", 1);
+}
+int main()
+{
+	__pid_t server_pid = getpid();
+	printf("%d\n", server_pid); //lembrar que nao posso usar o printf
+				signal(SIGUSR1, teste1);
+			while(1){
+			}
 }
