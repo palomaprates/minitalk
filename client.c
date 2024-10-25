7b9ed34879011a6a43c1cbd8fd5bf2eae9b6ca58
paloma <paloma.prates108@gmail.com>
2024-10-25 22:32:11 +0100

    client

--- client.c
+++ client.c
@@ -1,14 +1,31 @@
-#include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int	main(int argc, char *argv[])
 {
-	int	i;
-	i = 0;
-		if (argc == 2)
+	if (argc == 3)
+	{
+		int	i;
+		int	bit;
+		char	*server_pid;
+		char	ch; 
+		server_pid = argv[1];
+		ch = argv[2][0];
+		i = 7;
+		while (i >= 0) 
 		{
-			char *server_pid = argv[1];
-			kill(atoi(server_pid), SIGUSR1);
+			bit = (ch >> i) & 1;
+			if (bit == 0)
+			{
+				kill(atoi(server_pid), SIGUSR1);
+			}
+			else
+			{
+				kill(atoi(server_pid), SIGUSR2);
+			}
+			i--;
+			usleep(100);
 		}
+	}
 }
