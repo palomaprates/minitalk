a482f67788972d8491dc1eafd64638e74974a84b
palomaprates <paloma.prates108@gmail.com>
2025-01-11 11:39:03 -0300

    header

--- minitalk.h
+++ minitalk.h
@@ -1,7 +1,6 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 
-// #include "../ft_printf/ft_printf.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
@@ -9,7 +8,23 @@
 
 int	ft_strlen(const char *str);
 
-int	str_to_int(char *str);
+char	binary_to_char(char *str);
+
+int	binary_to_int(char *str);
+
+int	ft_atoi(const char *str);
+
+char	*ft_itoa(int n);
+
+void	*ft_memset(void *s, int c, int n);
+
+void	to_get_final_str(int signal, char **final_str, int *index);
+
+void	to_get_len(int signal, int *len);
+
+void	to_send_len(char *str, char *server_pid);
+
+void	to_send_ch(char *str, char *server_pid);
 
 void	handle_signal(int signal);
 
